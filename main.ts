13f4d29469efea95a7e65327ca32d81205c16c6c
tarirom-oss <tarirom@mpsc.co.za>
2026-04-16 12:40:08 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,15 @@
+let change_timer_by = 0
+basic.forever(function () {
+    let timer = 0
+    basic.showIcon(IconNames.Heart)
+    basic.pause(100)
+    change_timer_by += 1
+    if (timer) {
+    	
+    } else {
+    	
+    }
+})
+basic.forever(function () {
+	
+})
